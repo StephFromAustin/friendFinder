fa234bdbcaf23b03cdad6229d849ab4af788fb58
KSG Sounds Macbook Pro <ksgsound@StephGonza.attlocal.net>
2018-11-01 23:41:38 -0500

    /home route

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,11 @@
 // DEPENDENCIES 
 const path = require ('path');
 
+// ROUTE
+module.exports = function (app){
+    // HTML GET REQUEST.
+        //DIRECT USER TO HOME PAGE AND SURVEY PAGE.
+        app.get('/home', function (req, res){
+            res.sendFile(path.join(_dirname + '/../public/home.html'));
+        })
+}
