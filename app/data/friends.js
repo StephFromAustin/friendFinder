feea4d82291cbcdc76b3331c82b46268a292e9a0
KSG Sounds Macbook Pro <ksgsound@StephGonza.attlocal.net>
2018-11-01 23:25:38 -0500

    first field fix, needed commas between sub sections

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 let friends = [
     {
-        'name': 'Milton'
-        'photo': 'https://i.redd.it/0df96bwz24b11.jpg'
+        'name': 'Milton',
+        'photo': 'https://i.redd.it/0df96bwz24b11.jpg',
         'scores':[2,1,2,1,1,3,4,5,1,2]
     },
 ]
