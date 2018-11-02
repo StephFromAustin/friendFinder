081c2ded83f2e57396c79c7c101a511ed085a3f9
KSG Sounds Macbook Pro <ksgsound@StephGonza.attlocal.net>
2018-11-01 23:09:05 -0500

    set up express app to handle data parsing

--- app/server.js
+++ app/server.js
@@ -7,3 +7,9 @@ const app = express ();
 // SET PORT FOR APPLICATION 
 const PORT = process.env.PORT || 8080;
 
+// SET UP THE EXPRESS APP TO HANDLE DATA PARSING 
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
