49dd9beb227737199b83c876f2bc901d26ffe520
KSG Sounds Macbook Pro <ksgsound@StephGonza.attlocal.net>
2018-11-01 23:02:51 -0500

    body parser npm package installed

---
+++ app/server.js
@@ -0,0 +1,9 @@
+// DEPENDENCIES 
+const express = require('express'); // NODE.JS SERVER EXPRESS FRAMEWORK 
+const path = require ('path');
+
+// CONFIGURE EXPRESS 
+const app = express ();
+
+// SET PORT FOR APPLICATION 
+const PORT = process.env.PORT || 8080;
