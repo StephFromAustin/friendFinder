694045111b284fc420dabd125f3dc17c0a664d95
KSG Sounds Macbook Pro <ksgsound@StephGonza.attlocal.net>
2018-11-01 23:48:05 -0500

    function fix, function misspelled

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,12 +3,10 @@ const path = require ('path');
 
 // ROUTE
 module.exports = function (app){
-    // HTML GET REQUEST.
-        //DIRECT USER TO HOME PAGE AND SURVEY PAGE.
-        app.get('/home', function (req, res){
-            res.sendFile(path.join(_dirname + '/../public/home.html'));
-        });
-        app.get('/survey', function(req, res){
-            res.sendFile(path.join(_dirname + '/../public/survery.html'));
-
-})
+    app.get('/home', function (req, res){
+        res.sendFile(path.join(_dirname + '/../public/home.html'));
+    });
+    app.get('/survey', function (req, res){
+        res.sendFile(path.join(_dirname + '/../public/survey/html'));
+    })
+}
