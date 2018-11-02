71bf4b029a6a5fc918e037d4b5f11e250cdf043a
KSG Sounds Macbook Pro <ksgsound@StephGonza.attlocal.net>
2018-11-02 00:32:13 -0500

    starting API POST FUNCTION, not complete

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,4 +8,18 @@ module.exports = function(app) {
     app.get('/api/friends', function (req, res){
         res.json(friends);
     });
-}
+    // API POST 
+    app.get('/api/friends', function (req, res){
+        // VARIABLES 
+        let userRes = req.body;
+        let userScores = userRes.scores;
+            // USER MATCH VARIABLES 
+
+        // would a for loop work here? goes through friends.length and then userRes.length? NEED MATH SYNTAX!
+    
+    // PUSH AND RESPONSE FROM JSON 
+    friends.push(userRes);
+    // res.json({ SHOULD USER MATCH VAIRABLES GO HERE?})
+    
+    })
+};
