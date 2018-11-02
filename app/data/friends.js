a239b3642a45af4465069d865d6bf3d83c008f2c
KSG Sounds Macbook Pro <ksgsound@StephGonza.attlocal.net>
2018-11-02 00:40:11 -0500

    third friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -10,4 +10,9 @@ let friends = [
         'photo': 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwi81dDp_rTeAhVwm-AKHc23AkoQjRx6BAgBEAU&url=http%3A%2F%2Fwww.gosocial.co%2Fbette-midler-dressed-winnie-hocus-pocus-halloween%2F&psig=AOvVaw3izQ-opovpxA97UqsDv8e4&ust=1541223206314391',
         'scores':[1,1,1,1,3,4,5,1,1,2]
     }
+    {
+        'name': 'Dwight',
+        'photo': 'https://upload.wikimedia.org/wikipedia/en/c/cd/Dwight_Schrute.jpg',
+        'scores':[4,3,2,1,1,2,3,4,5,5]
+    }
 ]
