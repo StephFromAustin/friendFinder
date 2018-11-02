2910b500939596299c4398613f5ed73ad4218bbd
KSG Sounds Macbook Pro <ksgsound@StephGonza.attlocal.net>
2018-11-02 00:39:08 -0500

    second friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,4 +5,9 @@ let friends = [
         'photo': 'https://i.redd.it/0df96bwz24b11.jpg',
         'scores':[2,1,2,1,1,3,4,5,1,2]
     },
+    {
+        'name': 'Winnie',
+        'photo': 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwi81dDp_rTeAhVwm-AKHc23AkoQjRx6BAgBEAU&url=http%3A%2F%2Fwww.gosocial.co%2Fbette-midler-dressed-winnie-hocus-pocus-halloween%2F&psig=AOvVaw3izQ-opovpxA97UqsDv8e4&ust=1541223206314391',
+        'scores':[1,1,1,1,3,4,5,1,1,2]
+    }
 ]
