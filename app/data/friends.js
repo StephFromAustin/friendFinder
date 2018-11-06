5dcc0630a476d213e6fccbc16ec750f7775d10b7
KSG Sounds Macbook Pro <ksgsound@StephGonza.attlocal.net>
2018-11-06 00:27:04 -0600

    added hill family to the list of friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,22 @@
 // ARRAY OF DATA 
 let friends = [
     {
-        'name': 'Milton',
-        'photo': 'https://i.redd.it/0df96bwz24b11.jpg',
-        'bio': 'Here is a bio',
-        'scores': [2, 1, 2, 1, 1, 3, 4, 5, 1, 2]
+        'name': 'Hank Hill',
+        'photo': 'https://pbs.twimg.com/profile_images/529110027425308672/RUN7UTym.jpeg',
+        'bio': 'Hank lives with his wife Peggy, son Bobby, and number one dog Laaaddyybiirrrddd. Hank is a propane man and is assistant manager of Strickland Propane. Hank likes Alamo Beer,Football, Hanging out in the alley, and a nice beautiful lawn. ',
+        'scores': [5, 1, 4, 1, 2, 3, 5, 5, 5, 3]
     },
     {
-        'name': 'Winnie',
-        'photo': 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwi81dDp_rTeAhVwm-AKHc23AkoQjRx6BAgBEAU&url=http%3A%2F%2Fwww.gosocial.co%2Fbette-midler-dressed-winnie-hocus-pocus-halloween%2F&psig=AOvVaw3izQ-opovpxA97UqsDv8e4&ust=1541223206314391',
-        'scores': [1, 1, 1, 1, 3, 4, 5, 1, 1, 2]
+        'name': 'Peggy Hill',
+        'photo': 'https://vignette.wikia.nocookie.net/p__/images/6/6c/Peggy-hill-king-of-the-hill-28639963-210-240.jpg/revision/latest?cb=20141129214857&path-prefix=protagonist',
+        'bio': "Peggy is the wife of Hank and the mother of Bobby. Peggy is a two time substitute teacher of the year with Tom Landry Middle School and State of Texas Boggle Champion!",
+        'scores': [3, 3, 5, 1, 2, 1, 1, 5, 4, 2]
     },
     {
-        'name': 'Dwight',
-        'photo': 'https://upload.wikimedia.org/wikipedia/en/c/cd/Dwight_Schrute.jpg',
-        'bio': 'beets, beets, beets',
-        'scores': [4, 3, 2, 1, 1, 2, 3, 4, 5, 5]
+        'name': 'Bobby Hill',
+        'photo': 'https://uproxx.files.wordpress.com/2015/06/bobby-hill-chips.jpg?quality=95&w=650',
+        'bio': 'Bobby is the only child of Hank and Peggy Hill. Bobby is a budding prop comic, fierce purse protector, and natural rodeo clown. Bobby loves snacks, his family, and his neighbor Connie Souphanousinphone, Jr.',
+        'scores': [1, 5, 4, 3, 4, 3, 2, 3, 4, 5]
     }
 ]
  
