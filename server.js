ae8c900b065f7f9d2cc7e96597dc3e6149488876
KSG Sounds Macbook Pro <ksgsound@StephGonza.attlocal.net>
2018-11-05 21:01:24 -0600

    express.static path update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // CODE FOR CSS 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, '/app/public')));
 
 
 // ROUTES 
